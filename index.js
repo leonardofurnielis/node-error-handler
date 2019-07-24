5d7c0895f9ddddca179c29fed89eb154ed3f0097
Leonardo Furnielis <leonardo.furnielis1@ibm.com@Leonardos-MacBook-Pro.local>
2019-07-24 09:30:40 -0300

    update packages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strcit';
 
-const codeValidator = require('./src/code-validator');
-const status = require('./src/http-status');
+const codeValidator = require('./src/codeValidator');
+const status = require('./src/httpMessages');
 
 module.exports = env => {
   const production = env == 'production' ? true : false;
