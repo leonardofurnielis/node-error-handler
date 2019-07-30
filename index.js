b70b15669778f7cee2346c19c1520b933fb694f7
Leonardo Furnielis <leonardo.furnielis1@ibm.com@Leonardos-MacBook-Pro.local>
2019-07-30 16:36:30 -0300

    v1.0.1

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const codeValidator = require('./src/codeValidator');
 const status = require('./src/httpMessages');
 
 module.exports = env => {
-  const production = env === 'production';
-
   // eslint-disable-next-line no-unused-vars
   return (err, req, res, next) => {
     const statusCode = codeValidator(err.status) || 500;
@@ -18,10 +16,6 @@ module.exports = env => {
         details: err.message,
       },
     };
-
-    if (!production) {
-      console.error(err.stack);
-    }
 
     if (!err.message || err.message === '') {
       delete errorHandler.error.details;
